99668d4470078b4eb861832e2c0838bd018a7731
akulau <akulaurila@gmail.com>
2023-02-24 10:59:21 +0200

    siivottu projektia

--- backend/kirja-arkisto-api/src/app.js
+++ backend/kirja-arkisto-api/src/app.js
@@ -1,29 +1,17 @@
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
 const cors = require('cors');
-
-require('dotenv').config();
-
-const middlewares = require('./middlewares');
-const api = require('./api');
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+//Root
 app.get('/', (req, res) => {
+  res.statusCode = 200
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+    message: 'root',
   });
 });
-
-app.use('/api/v1', api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
 
 module.exports = app;
