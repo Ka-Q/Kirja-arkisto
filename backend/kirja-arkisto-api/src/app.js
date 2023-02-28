b6f2a4a336e7206d4b0a2864a1af177b51dd6fbb
Lauri Liukkonen <lauri.liukkonen@edu.savonia.fi>
2023-03-01 00:01:50 +0200

    Sarjojen lisäys

--- backend/kirja-arkisto-api/src/app.js
+++ backend/kirja-arkisto-api/src/app.js
@@ -12,6 +12,7 @@ const connect = (res, query, queryList) => {
     password: "root",
     database: "mydb",
     dateStrings: true,
+    
   });
   connection.query(query, queryList ,(error, SQLresult, fields) => {
     connection.end();
@@ -151,5 +152,115 @@ app.delete('/kirja', (req, res) => {
 
   connect(res, query, queryList)
 });
+
+app.get('/sarja', (req, res) => {
+
+  let listOfValues = []
+
+  let params = req.query;
+  let keys = Object.keys(params);
+  let query = "SELECT * FROM sarja WHERE (1=1)"
+  
+  for (let key in keys){
+    query += " AND ?? LIKE ?"
+    let avain = keys[key]
+    listOfValues.push(avain)
+    listOfValues.push(params[avain])
+  }
+
+  console.log(query)
+  connect(res, query, listOfValues)
+});
+
+app.post('/sarja', (req, res) => {
+  let params = req.body
+  let keys = Object.keys(params)
+  console.log(keys.length)
+
+  let listOfKeys = []
+  let listOfValues = []
+
+  let query = "INSERT INTO sarja ("
+
+  for (let key in keys){
+    query += "??,"
+    let avain = keys[key]
+    listOfKeys.push(avain)
+  }
+
+  query = query.substring(0, query.length - 1)
+
+  query += ") VALUES("
+
+  for (let key in keys){
+    query += "?,"
+    let avain = keys[key]
+    listOfValues.push(params[avain])
+  }
+
+  query = query.substring(0, query.length - 1)
+
+  query += ")"
+  let queryList = listOfKeys.concat(listOfValues);
+
+  console.log(query)
+
+  connect(res, query, queryList)
+})
+// DELETE method for sarja table
+app.delete('/sarja', (req, res) => {
+  let where = req.body;
+  let whereKeys = Object.keys(where);
+
+  let queryList = [];
+
+  let query = "DELETE FROM sarja WHERE (1=1)";
+
+  for (let x in whereKeys){
+    let key = whereKeys[x];
+    query += " AND ?? = ?";
+    queryList.push(key);
+    queryList.push(where[key]);
+  }
+
+  console.log(query);
+
+  connect(res, query, queryList);
+});
+
+app.put('/sarja', (req, res) => {
+  let set = req.body.set
+  let where = req.body.where
+
+  let setKeys = Object.keys(set)
+  let whereKeys = Object.keys(where)
+
+  let queryList = []
+
+  let query = "UPDATE sarja SET "
+
+  for (let x in setKeys) {
+    let key = setKeys[x];
+    query += "?? = ?,";
+    queryList.push(setKeys[x]);
+    queryList.push(set[key]);
+  }
+
+  query = query.substring(0, query.length - 1);
+
+  query += " WHERE (1=1)"
+
+  for (let x in whereKeys) {
+    let key = whereKeys[x];
+    query += " AND ?? = ?";
+    queryList.push(whereKeys[x]);
+    queryList.push(where[key]);
+  }
+
+  console.log(query)
+
+  connect(res, query, queryList)
+})
+
 
 module.exports = app;
