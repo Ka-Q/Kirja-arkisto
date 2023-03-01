f7496c581d728e3efd97f69dbf4bdfc4ba074ad4
akulau <akulaurila@gmail.com>
2023-03-01 10:47:06 +0200

    siirretty kirja ja lisätty tiedostoja funktioille

--- backend/kirja-arkisto-api/src/app.js
+++ backend/kirja-arkisto-api/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require("body-parser")
+const kirja_functions = require('./functions/kirja_functions')
 
 const app = express();
 app.use(bodyParser.json());
@@ -40,117 +41,24 @@ app.get('/', (req, res) => {
   });
 });
 
-
 app.get('/kirja', (req, res) => {
-
-    let listOfValues = []
-
-    let params = req.query;
-    let keys = Object.keys(params);
-    let query = "SELECT * FROM kirja WHERE (1=1)"
-    
-    for (let key in keys){
-      query += " AND ?? LIKE ?"
-      let avain = keys[key]
-      listOfValues.push(avain)
-      listOfValues.push(params[avain])
-    }
-
-    console.log(query)
-    connect(res, query, listOfValues)
+    let queryJson = kirja_functions.GetKirja(req);
+    connect(res, queryJson.query, queryJson.queryList)
 });
-
 
 app.post('/kirja', (req, res) => {
-  let params = req.body
-  let keys = Object.keys(params)
-  console.log(keys.length)
-
-  let listOfKeys = []
-  let listOfValues = []
-
-  let query = "INSERT INTO kirja ("
-
-  for (let key in keys){
-    query += "??,"
-    let avain = keys[key]
-    listOfKeys.push(avain)
-  }
-
-  query = query.substring(0, query.length - 1)
-
-  query += ") VALUES("
-
-  for (let key in keys){
-    query += "?,"
-    let avain = keys[key]
-    listOfValues.push(params[avain])
-  }
-
-  query = query.substring(0, query.length - 1)
-
-  query += ")"
-  let queryList = listOfKeys.concat(listOfValues);
-
-  console.log(query)
-
-  connect(res, query, queryList)
-})
-
-
-app.put('/kirja', (req, res) => {
-  let set = req.body.set
-  let where = req.body.where
-
-  let setKeys = Object.keys(set)
-  let whereKeys = Object.keys(where)
-
-  let queryList = []
-
-  let query = "UPDATE kirja SET "
-
-  for (let x in setKeys) {
-    let key = setKeys[x];
-    query += "?? = ?,";
-    queryList.push(setKeys[x]);
-    queryList.push(set[key]);
-  }
-
-  query = query.substring(0, query.length - 1);
-
-  query += " WHERE (1=1)"
-
-  for (let x in whereKeys) {
-    let key = whereKeys[x];
-    query += " AND ?? = ?";
-    queryList.push(whereKeys[x]);
-    queryList.push(where[key]);
-  }
-
-  console.log(query)
-
-  connect(res, query, queryList)
+  let queryJson = kirja_functions.PostKirja(req);
+  connect(res, queryJson.query, queryJson.queryList)
 });
 
+app.put('/kirja', (req, res) => {
+  let queryJson = kirja_functions.PutKirja(req);
+  connect(res, queryJson.query, queryJson.queryList)
+});
 
 app.delete('/kirja', (req, res) => {
-  let where = req.body
-  let whereKeys = Object.keys(where)
-
-  let queryList = []
-
-  let query = "DELETE FROM kirja WHERE (1=1)"
-
-  for (let x in whereKeys){
-    let key = whereKeys[x]
-    query += " AND ?? = ?"
-    queryList.push(key)
-    queryList.push(where[key])
-  }
-
-  console.log(query)
-
-  connect(res, query, queryList)
+  let queryJson = kirja_functions.DeleteKirja(req);
+  connect(res, queryJson.query, queryJson.queryList)
 });
 
 app.get('/sarja', (req, res) => {
