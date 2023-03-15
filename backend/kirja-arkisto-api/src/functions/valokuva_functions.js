9d415ca26cf0ad6c24431c0f69655f1cb1dc00b0
Lauri Liukkonen <lauri.liukkonen@edu.savonia.fi>
2023-03-15 08:57:38 +0200

    valokuva

---
+++ backend/kirja-arkisto-api/src/functions/valokuva_functions.js
@@ -0,0 +1,132 @@
+
+
+
+function GetValokuva(req) {
+
+  let returnJson = {}
+
+  let listOfValues = []
+  let params = req.query;
+  let keys = Object.keys(params);
+  let query = "SELECT * FROM valokuva WHERE (1=1)"
+
+  for (let key in keys) {
+    query += " AND ?? LIKE ?"
+    let avain = keys[key]
+    listOfValues.push(avain)
+    listOfValues.push(params[avain])
+  }
+
+  console.log(query)
+
+  returnJson.query = query;
+  returnJson.queryList = listOfValues
+
+  return returnJson
+}
+
+function PostValokuva(req) {
+  let params = req.body
+  let keys = Object.keys(params)
+  console.log(keys.length)
+
+  let listOfKeys = []
+  let listOfValues = []
+
+  let query = "INSERT INTO valokuva ("
+
+  for (let key in keys) {
+    query += "??,"
+    let avain = keys[key]
+    listOfKeys.push(avain)
+  }
+
+  query = query.substring(0, query.length - 1)
+
+  query += ") VALUES("
+
+  for (let key in keys) {
+    query += "?,"
+    let avain = keys[key]
+    listOfValues.push(params[avain])
+  }
+
+  query = query.substring(0, query.length - 1)
+
+  query += ")"
+  let queryList = listOfKeys.concat(listOfValues);
+
+  console.log(query)
+
+  let returnJson = {}
+  returnJson.query = query
+  returnJson.queryList = queryList
+  return returnJson
+}
+
+
+function PutValokuva(req) {
+  let set = req.body.set
+  let where = req.body.where
+
+  let setKeys = Object.keys(set)
+  let whereKeys = Object.keys(where)
+
+  let queryList = []
+
+  let query = "UPDATE valokuva SET "
+
+  for (let x in setKeys) {
+    let key = setKeys[x];
+    query += "?? = ?,";
+    queryList.push(setKeys[x]);
+    queryList.push(set[key]);
+  }
+
+  query = query.substring(0, query.length - 1);
+
+  query += " WHERE (1=1)"
+
+  for (let x in whereKeys) {
+    let key = whereKeys[x];
+    query += " AND ?? = ?";
+    queryList.push(whereKeys[x]);
+    queryList.push(where[key]);
+  }
+
+  console.log(query)
+
+  let returnJson = {}
+  returnJson.query = query
+  returnJson.queryList = queryList
+  return returnJson
+}
+
+function DeleteValokuva(req) {
+  let where = req.body
+  let whereKeys = Object.keys(where)
+
+  let queryList = []
+
+  let query = "DELETE FROM valokuva WHERE (1=1)"
+
+  for (let x in whereKeys) {
+    let key = whereKeys[x]
+    query += " AND ?? = ?"
+    queryList.push(key)
+    queryList.push(where[key])
+  }
+
+  console.log(query)
+
+  let returnJson = {}
+  returnJson.query = query
+  returnJson.queryList = queryList
+  return returnJson
+}
+
+
+exports.GetValokuva = GetValokuva
+exports.PutValokuva = PutValokuva
+exports.PostValokuva = PostValokuva
+exports.DeleteValokuva = DeleteValokuva
