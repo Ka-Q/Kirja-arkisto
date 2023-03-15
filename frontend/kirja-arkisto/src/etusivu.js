67108923e17cf1c23ec0bc5a4fc8c5f7b2a16efa
akulau <akulaurila@gmail.com>
2023-03-15 11:07:16 +0200

    testisivu

--- frontend/kirja-arkisto/src/etusivu.js
+++ frontend/kirja-arkisto/src/etusivu.js
@@ -6,43 +6,73 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from "react-bootstrap/esm/Button";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {LinkContainer} from 'react-router-bootstrap'
+import {Routes, Route, BrowserRouter as Router} from 'react-router-dom'
+import {Row, Col} from "react-bootstrap"
+
 
 function Etusivu () {
 
-
     return (
-        <div className="wrapper">
-            <Navbar bg="light" expand="lg" className="item-nav">
-        <Container>
-          <Navbar.Brand href="#home">Kirja-arkisto</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+      <div>
+      <Router>
+      <Navbar bg="light" expand="lg">
+          <Container>
+              <LinkContainer to="/"><Navbar.Brand>
+                  Kirjasovellus
+              </Navbar.Brand></LinkContainer>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mx-auto">
+                  <LinkContainer to="/"><Nav.Link className="mx-2" >Etusivu</Nav.Link></LinkContainer>
+                  <LinkContainer to="/kirja"><Nav.Link className="mx-2">Kirjat</Nav.Link></LinkContainer>
+                  <LinkContainer  to="/sarja"><Nav.Link className="mx-2">Sarjat</Nav.Link></LinkContainer>
+                  <LinkContainer to="/omakirja"><Nav.Link className="mx-2">Oma kirja</Nav.Link></LinkContainer>
+              </Nav>
+              </Navbar.Collapse>
+          </Container>
       </Navbar>
-            <div className="item-leftside">
-                <img src="https://dbdzm869oupei.cloudfront.net/img/sticker/preview/90751.png"/>
-            </div>
-            <div className="item-main">
-                <h1>Tervetuloa käyttämään kirja-arkistoa johon voit tallettaa omia kirjoja ja kirjasarjojasi</h1>
-            </div>
-        </div>
+
+      <Routes>
+          <Route path="/" element={<FrontPage/>}/>
+          <Route path="/kirja" element={<BookPage />}/>
+          <Route path="/sarja" element={<SeriesPage />}/>
+          <Route path="/omakirja" element={<OwnedBookPage/>}/>
+      </Routes>
+      </Router>
+  </div>
     )
+}
+
+const FrontPage = (props) => {
+  return (
+    <Row className="mx-5 my-5">
+      <Col>
+        <img src="https://dbdzm869oupei.cloudfront.net/img/sticker/preview/90751.png"/>
+      </Col>
+      <Col >
+        <h1  className="item-main">Tervetuloa käyttämään kirja-arkistoa johon voit tallettaa omia kirjoja ja kirjasarjojasi</h1>
+      </Col>
+    </Row>
+  )
+}
+
+const BookPage = (props) => {
+  return (
+    <>Kirja</>
+  )
+}
+
+const SeriesPage = (props) => {
+  return (
+    <>sarja</>
+  )
+}
+
+const OwnedBookPage = (props) => {
+  return (
+    <>oma kirja</>
+  )
 }
 
 
