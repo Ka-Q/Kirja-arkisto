69f5b0d01f0b422135ee24017c4c4ac556e4fbf3
akulau <akulaurila@gmail.com>
2023-03-17 14:12:55 +0200

    Lisätty pohja oman kirjan  lisäykselle + valokuvat

--- frontend/kirja-arkisto/src/omakirjasivu.js
+++ frontend/kirja-arkisto/src/omakirjasivu.js
@@ -4,30 +4,160 @@ import { Button, Card, Col, Row, Stack } from "react-bootstrap"
 
 const OmaKirjaSivu = () => {
 
-    const [listOfOwnBooks, setListOfOwnBooks] = useState([])
+    const [lisaaClicked, setLisaaClicked] = useState(false)
+    const [lisaaBtnText, setLisaaBtnText] = useState("Lisää oma kirja")
+
+
+    const handleLisaaClicked = () => {
+        
+        if (!lisaaClicked) {
+            setLisaaClicked(true)
+            setLisaaBtnText("Palaa omien kirjojen hakuun")
+        } else {
+            setLisaaClicked(false)
+            setLisaaBtnText("Lisää oma kirja")
+        }
+    }
 
     return (
-        <div className="mx-5">
+        <div className="mx-3">
             <div className="text-center" style={{marginTop: "2em"}}>
                 <h1>Oma kirja</h1>
             </div>
             <Row>
                 <Col>
                     <Stack direction="horizontal" gap={3}>
-                        <div className="bg-light border ms-auto"><Button >Lisää</Button></div>
-                        <div className="bg-light border"><Button>Muokkaa</Button></div>
-                        <div className="bg-light border"><Button>Poista</Button></div>
+                        <div className="bg-light border ms-auto"><Button onClick={(e) => handleLisaaClicked(e.target)}>{lisaaBtnText}</Button></div>
+                        {/*<div className="bg-light border"><Button>Muokkaa</Button></div>
+                        <div className="bg-light border"><Button>Poista</Button></div>*/}
                     </Stack>
                 </Col>
             </Row>
-            <Row className="mt-3" >
-                <Col>
-                <div className="text-center" style={{verticalAlign: "center", lineHeight: "2.3em"}}>
-                    <SearchBar list={listOfOwnBooks} setList={setListOfOwnBooks}/>
-                </div>
-                </Col>
-            </Row>
+            {
+            !lisaaClicked ?
+                <Row className="mt-3" >
+                    <Col>
+                    <div className="text-center" style={{verticalAlign: "center", lineHeight: "2.3em"}}>
+                        <SearchBar />
+                    </div>
+                    </Col>
+                </Row>
+            :
+                <Row className="mt-3" >
+                    <Col>
+                    <div className="text-center" style={{verticalAlign: "center", lineHeight: "2.3em"}}>
+                        <AddComponent handleLisaaClicked={handleLisaaClicked}/>
+                    </div>
+                    </Col>
+                </Row>
+            }
         </div>
+    )
+}
+
+const AddComponent = (props) => {
+
+    const [kuntoluokka, setKuntoluokka] = useState(-1);
+    const [hankintahinta, sethankintahinta] = useState(-1);
+    const [esittelyteksti, setEsittelyteksti] = useState("");
+    const [painosvuosi, setPainosvuosi] = useState(-1);
+    const [hankintaAika, setHankintaAika] = useState(new Date);
+    const [kirjaId, setKirjaId] = useState({});
+    const [valokuvat, setValokuvat] = useState([]);
+
+    const [addPicComponents, setAddPicComponents] = useState([]);
+    const [addPicKeys, setAddPicKeys] = useState(0);
+
+    const inputStyle = {width: "60%", paddingLeft: "1em"}
+
+    const handleDeletePicClicked = () => {
+        let list = addPicComponents.slice(0, addPicComponents.length-1)
+        setAddPicComponents(list)
+    }
+
+    const handleAddPictureClicked = (e) => {
+        setAddPicKeys(addPicKeys + 1)
+        setAddPicComponents([...addPicComponents, <AddPictureComponent 
+            key={addPicComponents.length} 
+            inputStyle={inputStyle} />])
+    }
+
+    return (
+        <Card border="dark" className="mb-1">
+            <Card.Body>
+                <Card.Title>Lisää uusi Oma Kirja</Card.Title>
+                <Row className="mb-2">
+                    <Col>
+                    <Stack direction="vertical" gap={3} style={{textAlign: "center"}}>
+                        <div>Valitse kirja (jos kirjaa ei löydy listasta, voit lisätä sellaisen <a href="http://localhost:3000/kirja">täältä</a>):</div>
+                        <div> 
+                            <select onChange={(e) => setKirjaId(e.target.value)} style={inputStyle}>
+                                <option value={0}>--Kirja--</option>
+                            </select> 
+                        </div>
+                        <div>
+                            <input onChange={(e) => setKuntoluokka(e.target.value)} type="number" placeholder="kuntoluokka" style={inputStyle}/>
+                        </div>
+                        <div>
+                            <input onChange={(e) => sethankintahinta(e.target.value)} placeholder="hankintahinta" style={inputStyle}/>
+                        </div>
+                        <div>
+                            <textarea onChange={(e) => setEsittelyteksti(e.target.value)} placeholder="esittelyteksti" style={inputStyle}/>
+                        </div>
+                        <div>
+                            <input onChange={(e) => setPainosvuosi(e.target.value)} type="number" placeholder="painosvuosi" style={inputStyle}/> 
+                        </div>
+                        <div>hankinta-aika:</div>
+                        <div>
+                            <input onChange={(e) => setHankintaAika(e.target.value)} type="date" style={inputStyle}/>
+                        </div>
+                        
+                    </Stack>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div>valokuvat:</div>
+                        <div>
+                            {addPicComponents}
+                            {addPicComponents.length > 0?
+                            <Row  className="mb-3">
+                                <Col>
+                                    <Button variant="danger" onClick={(e) => handleDeletePicClicked()}>Poista valokuva</Button>
+                                </Col>
+                            </Row>
+                            : <></>}
+                            <hr/>
+                            <Button onClick={(e) => handleAddPictureClicked(e)}>+ Lisää uusi valokuva</Button>
+                        </div>
+                        <div className="my-5">
+                            <Button>Tallenna</Button> <Button onClick={(e) => props.handleLisaaClicked()}>Peruuta</Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    )
+}
+
+const AddPictureComponent = (props) => {
+    const inputStyle = props.inputStyle
+
+    const [tiedosto, setTiedosto] = useState("");
+    const [nimi, setNimi] = useState("");
+    const [sivunumero, setSivunumero] = useState("");
+
+    return (
+        <Row className="mt-2 mb-3">
+            <hr/>
+            <Col>
+                <Stack direction="vertical" gap={3} style={{textAlign: "center"}}>
+                    <div><input onChange={(e) => setTiedosto(e.target.value)} type={"file"} style={inputStyle}/></div>
+                    <div><input onChange={(e) => setNimi(e.target.value)} type={"text"} placeholder="nimi/kuvaus" style={inputStyle}/></div>
+                    <div><input onChange={(e) => setSivunumero(e.target.value)} type={"number"} placeholder="sivunumero" style={inputStyle}/></div>
+                </Stack>
+            </Col>
+        </Row>
     )
 }
 
@@ -141,7 +271,7 @@ const BookCard = (props) => {
                     </Col>
                     <Col>
                         <Card.Text>
-                            Kuntoluokka: {omakirja.kuntoluokka} <p> </p>
+                            Kuntoluokka: {omakirja.kuntoluokka} <br/>
                             Hankittu: {omakirja.hankinta_aika}
                         </Card.Text>
                     </Col>
