96ea86ae1c21b827376a00bcdb067ba372167e3c
akulau <akulaurila@gmail.com>
2023-04-03 17:58:34 +0300

    ViewOmakirja nyt vähän fiksumman näköinen

--- frontend/kirja-arkisto/src/omakirja/viewOmaKirja.js
+++ frontend/kirja-arkisto/src/omakirja/viewOmaKirja.js
@@ -1,6 +1,6 @@
-import { useState } from "react"
-import { Button, Card, Col, Image, Row, Stack } from "react-bootstrap"
-import { getCoverArt } from "./utilityFunctions"
+
+import { Card, Col, Row } from "react-bootstrap"
+import { KuvaViewerComponent, ValokuvaViewerComponent } from "./kuvaComponents"
 
 const ViewComponent = (props) => {
     let omakirja = props.omakirja
@@ -8,30 +8,47 @@ const ViewComponent = (props) => {
     let kuvat = kirja.kuvat
     let valokuvat = omakirja.valokuvat
 
+    console.log(valokuvat);
+
+    let kuntoluokkaStars = "";
+    for (let i = 0; i < omakirja.kuntoluokka; i++) {
+        kuntoluokkaStars += "⭐"
+    }
+
     return(
         <>
         <Row>
-            <Col>
-            <h1>{kirja.nimi}</h1>
-            <p>
-                {omakirja.toString()}
-            </p>
-            </Col>
-            <Col sm={3}>
+        <Col sm={3}>
                 <Card>
                     <Card.Title>
                         Kuvat
                     </Card.Title>
                     <Card.Body>
-                        <ImageViewerComponent kuvat={kuvat}/>
+                        <KuvaViewerComponent kuvat={kuvat}/>
                     </Card.Body>
                 </Card>
+            </Col>
+            <Col>
+                <h1>{kirja.nimi}</h1>
+                <Card>
+
+                    <Card.Title className="mt-3">Kuntoluokka: {kuntoluokkaStars}  <br/></Card.Title>
+                    <Card.Body>
+                        
+                        Painettu vuonna {omakirja.painosvuosi} <br/>
+                        Hankittu {omakirja.hankinta_aika} <br/>
+                        Hankintahinta {omakirja.hankintahinta} € <br/>
+                        Esittely: {omakirja.esittelyteksti} 
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col sm={3}>
                 <Card>
                     <Card.Title>
                         Valokuvat
                     </Card.Title>
                     <Card.Body>
-                        <ImageViewerComponent kuvat={[]}/>
+                        <ValokuvaViewerComponent valokuvat={valokuvat}/>
                     </Card.Body>
                 </Card>
             </Col>
@@ -39,88 +56,5 @@ const ViewComponent = (props) => {
         </>
     )
 }
-
-const ImageViewerComponent = (props) => {
-
-    // Tietoa esikatselulle ja pikkukuvien kelaukselle
-    const [clickedPic, setClickedPic] = useState(props.kuvat[0]);
-    const [rollIndex, setRollIndex] = useState(0);
-
-    const width = 5
-
-    // Jos kirjalla ei ole kuvia, palautetaan tyhjä komponentti
-    if (!props.kuvat || props.kuvat.length == 0) return (<></>)
-
-    let kuvaSrc = "http://localhost:5000/kuvatiedosto?kuva=";
-    let kuvat = props.kuvat
-
-    // kelaavat pikkukuvia nappeja painettaessa
-    const handleIncrease = () => {
-        if (rollIndex < kuvat.length - width) setRollIndex(rollIndex + 1);
-    }
-    const handleDecrease = () => {
-        if (rollIndex > 0) setRollIndex(rollIndex - 1);
-    }
-
-    // Järjestetään kuvat tyypin mukaan (etukansi, takakansi, muut)
-    let kuvatSorted = sortByTyyppi(kuvat)
-
-    // Rajataan näkyviin tulevat pikkukuvat indeksin ja leveyden perusteella
-    let croppedlist = kuvatSorted.slice(rollIndex, rollIndex + width)
-
-    // Mapataan rajattu lista pikkukuviksi
-    let previewList = mapToPreviews(croppedlist, kuvaSrc, clickedPic, setClickedPic)
-    
-    return(
-        <div style={{width:"25em"}}>
-            <div className="mx-auto" style={{width:"25em", height: "40em"}}>
-                <a onClick={(e) => window.open(kuvaSrc + clickedPic.kuva, '_blank').focus()} style={{cursor:"pointer"}}>
-                <Image src={kuvaSrc + clickedPic.kuva} fluid style={{width:"100%"}}/>
-                </a>
-            </div>
-            <Stack direction="horizontal" gap={1}>
-                <Button onClick={(e) => handleDecrease()}> {"< " + rollIndex} </Button>
-                {previewList}
-                <Button onClick={(e) => handleIncrease()}> {"> " + (kuvat.length - rollIndex - width)} </Button>
-            </Stack>
-            <div style={{textAlign: "left"}}>
-                Tyyppi: {clickedPic.kuva_tyyppi_id}<br/>
-                Taiteilija: {clickedPic.taiteilija} ({clickedPic.julkaisuvuosi}) <br/>
-                Tyyli: {clickedPic.tyyli} <br/>
-                Kuvaus: {clickedPic.kuvaus} <br/>
-            </div>
-        </div>
-    )
-}
-
-const sortByTyyppi = (kuvat) => {
-    return kuvat.sort((a, b) => {
-        if (a.kuva_tyyppi_id < b.kuva_tyyppi_id) return -1
-        if (a.kuva_tyyppi_id > b.kuva_tyyppi_id) return 1
-        return 0
-    });
-}
-
-const mapToPreviews = (list, kuvaSrc, clickedPic, setClickedPic) => {
-    let previewList = list.map((n, index) => {
-        let style = {width: "20%"};
-        let clickedStyle = {width: "20%", border: "2px solid black", borderRadius: "5px"}
-        
-        if (clickedPic.kuva_id == n.kuva_id){
-            return (
-                <div key={index} onClick={(e) => setClickedPic(n)} style={clickedStyle}>
-                <Image src={kuvaSrc + n.kuva} thumbnail fluid/>
-            </div>
-            )
-        }
-        return (
-            <div key={index} onClick={(e) => setClickedPic(n)} style={style}>
-                <Image src={kuvaSrc + n.kuva} thumbnail fluid/>
-            </div>
-        )
-    });
-    return previewList
-}
-
 
 export {ViewComponent}
