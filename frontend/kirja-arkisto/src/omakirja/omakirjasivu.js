0d33901ce12f00aa94f40d7a9c650f1e6d8717d5
akulau <akulaurila@gmail.com>
2023-04-05 16:04:32 +0300

    Tummaa tyyliä oman kirjan sivuille

--- frontend/kirja-arkisto/src/omakirja/omakirjasivu.js
+++ frontend/kirja-arkisto/src/omakirja/omakirjasivu.js
@@ -40,12 +40,12 @@ const OmaKirjaSivu = () => {
     }
 
     return (
-
-        <div className="mx-3 mt-5">
+        <div  style={{backgroundColor: "#202020"}}>
+        <div className="mx-3 pt-5">
             <Row>
                 <Col>
                     <Stack direction="horizontal" gap={3}>
-                        <div className="bg-light border ms-auto"><Button onClick={(e) => handleButtonClicked(e.target)}>{btnText}</Button></div>
+                        <div className="ms-auto"><Button className='btn btn-dark' style={{backgroundColor: "#424242"}} onClick={(e) => handleButtonClicked(e.target)}>{btnText}</Button></div>
                     </Stack>
                 </Col>
             </Row>
@@ -76,6 +76,7 @@ const OmaKirjaSivu = () => {
                 </Col>
             </Row>
             }
+        </div>
         </div>
     )
 }
@@ -205,16 +206,16 @@ const SearchComponent = (props) => {
     return (
         <div>
             <div className="text-center">
-                <h1>Oma kirja</h1>
+                <h1 style={{color: "white"}}>Oma kirja</h1>
             </div>
             <div className="text-center" style={{verticalAlign: "center", lineHeight: "2.3em"}}>
-                <input type={"search"} onChange={(e) => setNimi(e.target.value)} style={{width: "65%", paddingLeft: "1em"}} placeholder="Hae omista kirjoista"></input>
-                <Button onClick={handleSearchClick} style={{width: "3.5em", height: "3.5em", marginLeft: "1em"}}>🔎</Button>
-                <Button onClick={handleViewModeClick} style={{width: "3.5em", height: "3.5em", marginLeft: "1em"}}>{viewModeIcon}</Button>
+                <input type={"search"} onChange={(e) => setNimi(e.target.value)} style={{width: "65%", paddingLeft: "1em", backgroundColor:"#3a3a3a", borderRadius: '100px', color: "white" }} placeholder="Hae omista kirjoista"></input>
+                <Button onClick={handleSearchClick} className='btn btn-dark' style={{width: "3.5em", height: "3.5em", marginLeft: "1em", backgroundColor: "#424242"}}>🔎</Button>
+                <Button onClick={handleViewModeClick} className='btn btn-dark'  style={{width: "3.5em", height: "3.5em", marginLeft: "1em", backgroundColor: "#424242"}}>{viewModeIcon}</Button>
                 <div  className="mx-5" style={{marginTop: "3em", marginBottom: "25em"}}>
                     {BookCardList}
                 </div>
-                <Logincomponent/>
+                {/*<Logincomponent/>*/}
             </div>
 
         </div>
@@ -227,6 +228,7 @@ const Logincomponent = () => {
     const [salasana, setSalasana] = useState("");
     const [clickCounter, setClickCounter] = useState(0);
     const [clickCounter2, setClickCounter2] = useState(0);
+    const [clickCounter3, setClickCounter3] = useState(0);
 
     useEffect(() => {
         const sendAuth = async () => {
@@ -256,12 +258,27 @@ const Logincomponent = () => {
         if (clickCounter2 > 0) sendAuth();
     }, [clickCounter2]);
 
+    useEffect(() => {
+        const sendAuth = async () => {
+            const f = await fetch("http://localhost:5000/check_login", {
+                credentials: "include",
+                method: 'GET'
+            })
+            const data = await f.json();
+            console.log(data.data);
+        };
+        if (clickCounter3 > 0) sendAuth();
+    }, [clickCounter3]);
+
 
     const handleClick = () => {
         setClickCounter(clickCounter + 1);
     }
     const handleClick2 = () => {
         setClickCounter2(clickCounter2 + 1);
+    }
+    const handleClick3 = () => {
+        setClickCounter3(clickCounter3 + 1);
     }
 
     return (
@@ -270,6 +287,7 @@ const Logincomponent = () => {
             <input placeholder="salasana" onChange={(e) => setSalasana(e.target.value)}/>
             <button onClick={(e) => handleClick()}>Kirjaudu</button>
             <button onClick={(e) => handleClick2()}>Kirjaudu ulos</button>
+            <button onClick={(e) => handleClick3()}>/check_login</button>
         </>
 
     )
