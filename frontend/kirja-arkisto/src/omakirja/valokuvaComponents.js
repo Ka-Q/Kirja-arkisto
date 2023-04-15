6b9189e8e93e937bfde89fdd13ce5dd880f9bf30
akulau <akulaurila@gmail.com>
2023-04-15 16:14:07 +0300

    Lisätty omalle kirjalle valokuvien lisäys, muokkaus ja poisto

--- frontend/kirja-arkisto/src/omakirja/valokuvaComponents.js
+++ frontend/kirja-arkisto/src/omakirja/valokuvaComponents.js
@@ -1,7 +1,8 @@
-import { useState} from "react"
-import { Button, Image, Stack, Row, Col , Card } from "react-bootstrap"
+import { useEffect, useState} from "react"
+import { Button, Image, Stack, Row, Col , Card, Collapse } from "react-bootstrap"
 import theme from './theme.json'
-import { RequiredComponent } from "./utlilityComponents"
+import { RequiredComponent, WarningComponent } from "./utlilityComponents"
+import { sendValokuvaForm } from "./utilityFunctions"
 
 // Järjestää annetun listan valokuvia niiden sivunumeron mukaan kasvavasti
 const sortBySivunumero = (valokuvat) => {
@@ -66,8 +67,9 @@ const mapValokuvaToPreviews = (list, kuvaSrc, clickedPic, setClickedPic) => {
         n.index = index
         let sivunro = n.sivunumero
 
-        if (sivunro == -200) sivunro = "etukansi"
-        if (sivunro == -100) sivunro = "takakansi"
+        if (sivunro == -200) sivunro = "Etu📘"
+        if (sivunro == -100) sivunro = "Taka📘"
+        if (sivunro >= 0) sivunro = "📖" + sivunro
 
         if (clickedPic.valokuva_id == n.valokuva_id){
             return (
@@ -79,7 +81,7 @@ const mapValokuvaToPreviews = (list, kuvaSrc, clickedPic, setClickedPic) => {
         return (
             <div id={"previewPic" + index} className="mx-1" key={index} onClick={(e) => setClickedPic(n)} style={{position: "relative", display:"inline-block", overflow: "auto", whiteSpace: "nowrap", height: "7em" , borderRadius: "0.3em"}}>
                 <div style={{display: "inline-block", overflow: "auto", whiteSpace: "nowrap", height: "94%", filter: "brightness(0.4)", margin: 0, padding: 0}}><Image src={kuvaSrc + n.valokuva} height={"100%"}/></div>
-                <div className="px-2" style={{position: "absolute", width: "auto", height: "auto", top: "-0.5em", right: "0.5em", paddingTop: "0.4em", color: "white", backgroundColor: "rgba(75, 75, 75 , 0.77)", borderRadius: "0.5em"}}>
+                <div className="px-1" style={{position: "absolute", width: "auto", height: "auto", top: "-0.5em", right: "0.2em", paddingTop: "0.4em", color: "white", backgroundColor: "rgba(75, 75, 75 , 0.77)", borderRadius: "0.5em", fontSize: "1em"}}>
                     {sivunro}
                 </div>
             </div>
@@ -92,11 +94,20 @@ const mapValokuvaToPreviews = (list, kuvaSrc, clickedPic, setClickedPic) => {
 const ValokuvaViewerComponent = (props) => {
 
     // Tietoa esikatselulle ja pikkukuvien kelaukselle
-    const [clickedPic, setClickedPic] = useState(props.valokuvat[0]);
-    const [valokuvaList, setValokuvaList] = useState(sortBySivunumero(props.valokuvat))
+    const [clickedPic, setClickedPic] = useState(props.omakirja.valokuvat[0]);
+    const [valokuvaList, setValokuvaList] = useState(sortBySivunumero(props.omakirja.valokuvat))
+    const [deleteClicked, setDeleteClicked] = useState(false)
+    const [editClicked, setEditClicked] = useState(false)
+    const [addClicked, setAddClicked] = useState(false);
+
+    useEffect(() => {
+        setDeleteClicked(false)
+        setEditClicked(false)
+        setAddClicked(false)
+    }, [clickedPic])
 
     // Jos oamlla kirjalla ei ole valokuvia, palautetaan tyhjä komponentti
-    if (!props.valokuvat || props.valokuvat.length == 0) return (<></>)
+    if (!props.omakirja.valokuvat || props.omakirja.valokuvat.length == 0) return (<></>)
 
     let kuvaSrc = "http://localhost:5000/valokuvatiedosto?valokuva=";
 
@@ -135,9 +146,27 @@ const ValokuvaViewerComponent = (props) => {
     if (sivunumero == -200) sivunumero = "etukansi"
     if (sivunumero == -100) sivunumero = "takakansi"
 
-    let btnStyle = {backgroundColor: "rgba(40,40,40,0.8)", width:"4em", height: "4em", borderRadius: "1em", padding: "1em", cursor: "pointer", userSelect: "none", fontWeight: "bold", position: "absolute", top: "40%"}
+    let btnStyle = {backgroundColor: "rgba(40,40,40,0.8)", width:"4em", height: "4em", borderRadius: "1em", padding: "1em", cursor: "pointer", userSelect: "none", fontWeight: "bold", position: "absolute", top: "20em"}
     let BtnStyleLeft = JSON.parse(JSON.stringify(btnStyle)); BtnStyleLeft.left = 0
     let BtnStyleRight = JSON.parse(JSON.stringify(btnStyle)); BtnStyleRight.right = 0
+
+    const handleDeleteClicked = () => {
+        setEditClicked(false)
+        setAddClicked(false)
+        setDeleteClicked(!deleteClicked)
+    }
+    
+    const handleEditClicked = () => {
+        setDeleteClicked(false)
+        setAddClicked(false)
+        setEditClicked(!editClicked)
+    }
+
+    const handleAddClicked = () => {
+        setEditClicked(false)
+        setDeleteClicked(false)
+        setAddClicked(!addClicked)
+    }
 
     return(
         <Card border="secondary" style={{backgroundColor: theme.accent, color: "white"}}>
@@ -157,15 +186,188 @@ const ValokuvaViewerComponent = (props) => {
                 </div>
                 <hr/>
                 <Stack direction="horizontal" gap={0} className="me-2 my-2" style={{position: "relative", top :0}}>
-                    <Button className="ms-auto" variant="success" style={{backgroundColor: theme.accent}}  onClick={(e) => console.log("muokataan")}>✏</Button> <span className="mx-1"/>
-                    <Button variant="danger" style={{backgroundColor: theme.accent}} onClick={(e) => console.log("poistetaan")}>🗑</Button>
-                    <span className="mx-3" style={{lineHeight: "2.3em", width: "0.2em", backgroundColor: theme.input, color: theme.input, borderRadius: "1em"}}>|</span>
-                    <Button variant="primary" style={{backgroundColor: theme.accent}} onClick={(e) => console.log("lisätään")}>➕</Button>
+                    <div>Valokuva:</div>
+                    <Button className="ms-auto" variant="success" style={{backgroundColor: theme.accent}}  onClick={(e) => handleEditClicked()} aria-expanded={editClicked} aria-controls="editValokuva">✏</Button> <span className="mx-1"/>
+                    <Button variant="danger" style={{backgroundColor: theme.accent}} onClick={(e) => handleDeleteClicked()} aria-expanded={deleteClicked} aria-controls="deleteValokuva">🗑</Button>
+                    <span className="mx-3" style={{lineHeight: "2.3em", width: "0.2em", backgroundColor: theme.input, color: theme.input, borderRadius: "1em", userSelect: "none"}}>|</span>
+                    <Button variant="primary" style={{backgroundColor: theme.accent}} onClick={(e) => handleAddClicked()} aria-expanded={addClicked} aria-controls="addValokuva">➕</Button>
                 </Stack>
-                Sivunumero: {sivunumero} <br/>
-                Nimi/kuvaus: {clickedPic.nimi}
+                <Collapse in={deleteClicked}>
+                    <div id="deleteValokuva">
+                        <DeleteValokuvaComponent setDeleteClicked={setDeleteClicked} clickedPic={clickedPic} sivunumero={sivunumero}/>
+                    </div>
+                </Collapse>
+                <Collapse in={editClicked}>
+                    <div id="editValokuva">
+                        <EditValokuvaComponent setEditClicked={setEditClicked} clickedPic={clickedPic}/>
+                    </div>
+                </Collapse>
+                <Collapse in={addClicked}>
+                    <div id="addValokuva">
+                        <AddValokuvaComponent setAddClicked={setAddClicked} omakirjaId={props.omakirja.oma_kirja_id}/>
+                    </div>
+                </Collapse>
+                <Collapse in={!deleteClicked && !editClicked && !addClicked}>
+                    <Row className="my-3">
+                        <Col>Sivunumero:  <br/>{sivunumero}</Col>
+                        <Col>Nimi/kuvaus: <br/>{clickedPic.nimi}</Col>
+                    </Row>
+                </Collapse>
             </Card.Body>
         </Card>
+
+    )
+}
+
+const EditValokuvaComponent = (props) => {
+
+    const [sivunumeroOriginal, setSivunumeroOriginal] = useState(props.clickedPic.sivunumero)
+    const [nimiOriginal, setNimiOriginal] = useState(props.clickedPic.nimi)
+
+    const [sivunumero, setSivunumero] = useState(sivunumeroOriginal)
+    const [nimi, setNimi] = useState(nimiOriginal)
+
+    const [showSivu, setShowSivu] = useState(sivunumeroOriginal >= 0)
+
+    let sivunumeroChanged = sivunumeroOriginal != sivunumero
+    let nimiChanged = nimiOriginal != nimi
+
+    // Onko mitään muutettu?
+    let isChanged = sivunumeroChanged || nimiChanged
+
+    const handleSave = async () => {
+        if (isChanged) {
+            let updateObject = {
+                where: {
+                    valokuva_id: props.clickedPic.valokuva_id
+                },
+                set: {
+                    sivunumero: sivunumero,
+                    nimi: nimi
+                }
+            }
+            console.log(updateObject)
+            const f = await fetch("http://localhost:5000/valokuva", {
+                method: "PUT",
+                credentials: "include",
+                body: JSON.stringify(updateObject),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const data = await f.json()
+            console.log(data.data);
+            window.location.reload()    // Päivitetään sivu, jotta tehdyt muutokset tulevat näkyviin
+            
+        } else {
+            props.setEditClicked(false) // Jos ei muutoksia, niin ei tallenneta
+        }
+    }
+
+    const inputStyle = {width: "100%", paddingLeft: "1em", paddingRight: "1em", borderRadius: '100px', color: "white", backgroundColor: theme.input, lineHeight: "2.3em"}
+
+    const handleEtukansi = () => {
+        setShowSivu(false)
+        setSivunumero(-200)
+    }
+
+    const handleTakakansi = () => {
+        setShowSivu(false)
+        setSivunumero(-100)
+    }
+
+    const handleSivu = () => {
+        setShowSivu(true)
+        setSivunumero(0)
+    }
+
+    return(
+        <div>
+            <h5>Muokataan valokuvan tietoja</h5> 
+            <div>
+                <label for="etukansiRadio" className="pe-1" style={{width: "6em"}} onClick={(e) => handleEtukansi()}>Etukansi</label> 
+                <input id="etukansiRadio" type="radio" name="type" value="etukansi" defaultChecked={sivunumeroOriginal == -200} onClick={(e) => handleEtukansi()}/>
+            </div>
+            <div className="mx-5">
+                <label for="takakansiRadio" className="pe-1" style={{width: "6em"}} onClick={(e) => handleTakakansi()}>Takakansi</label> 
+                <input id="takakansiRadio" type="radio" name="type" value="takakansi" defaultChecked={sivunumeroOriginal == -100} onClick={(e) => handleTakakansi()}/>
+                {sivunumeroChanged? <span style={{position:"absolute", right: "5em", color: "orange"}}>*</span>:<span style={{paddingLeft: "2em", position:"absolute"}}/>}
+            </div>
+            <div>
+                <label for="sivuRadio" className="pe-1" style={{width: "6em"}} onClick={(e) => handleSivu()}>Sivu</label> 
+                <input id="sivuRadio" type="radio" name="type" value="sivu" defaultChecked={sivunumeroOriginal >= 0} onClick={(e) => handleSivu()}/>
+            </div>
+            {showSivu?
+                <input className="mt-3" onChange={(e) => setSivunumero(e.target.value)} value={sivunumero} type="number" placeholder="sivunumero" style={inputStyle}/>
+            : <></>}
+            <input onChange={(e) => setNimi(e.target.value)} value={nimi} className="my-3" type="text" placeholder="nimi/kuvaus" style={inputStyle}/>
+            <div>
+                <Button variant="warning" onClick={(e) => props.setEditClicked(false)}>Peruuta</Button>
+                <span className="mx-2"/>
+                <Button variant="success" onClick={(e) => handleSave()}>Tallenna</Button>
+            </div>
+        </div>
+    )
+}
+
+const DeleteValokuvaComponent = (props) => {
+
+    const handleDelete = async () => {
+        let clickedPic = props.clickedPic
+        const f = await fetch("http://localhost:5000/oman_kirjan_valokuvat" , {
+            method: "DELETE",
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({valokuva_id: clickedPic.valokuva_id})
+        });
+        const data = await f.json();
+        
+        const f2 = await fetch("http://localhost:5000/valokuva" , {
+            method: "DELETE",
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({valokuva_id: clickedPic.valokuva_id})
+        });
+        const data2 = await f2.json();
+        window.location.reload();
+    }
+
+    return(
+        <div>
+            <WarningComponent text={`Haluatko varmasti poistaa tämän valokuvan omalta kirjaltasi? Sivu: ${props.sivunumero}?`}/>
+            <div>
+                <Button variant="warning" onClick={(e) => props.setDeleteClicked(false)}>Peruuta</Button>
+                <span className="mx-2"/>
+                <Button variant="danger" onClick={(e) => handleDelete()}>Poista</Button>
+            </div>
+        </div>
+    )
+}
+
+const AddValokuvaComponent = (props) => {
+
+    const handleSave = async () => {
+        let success = await sendValokuvaForm(document.getElementById("picForm0"), props.omakirjaId)
+        if (success) window.location.reload()
+    }
+
+    const inputStyle = {width: "100%", paddingLeft: "1em", paddingRight: "1em", borderRadius: '100px', color: "white", backgroundColor: theme.input, lineHeight: "2.3em"}
+
+    return(<div>
+                <h5>Lisää valokuva</h5> 
+                <AddValokuvaFormComponent 
+                    inputStyle={inputStyle}
+                    formId={"picForm0"}
+                /> 
+                <Button variant="warning" onClick={(e) => props.setAddClicked(false)}>Peruuta</Button>
+                <span className="mx-2"/>
+                <Button variant="primary" onClick={(e) => handleSave()}>Tallenna</Button>
+
+            </div>
     )
 }
 
