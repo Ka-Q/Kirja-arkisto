3dda5052fa03074c27f70e41332cb1c5ed12e3d7
Oskari Kärkkäinen <Oskari.Karkkainen@edu.savonia.fi>
2023-04-20 10:55:15 +0300

    editomasarja muokkauksia

--- frontend/kirja-arkisto/src/omasarja/editOmaSarja.js
+++ frontend/kirja-arkisto/src/omasarja/editOmaSarja.js
@@ -1,52 +1,62 @@
 import { useState, useEffect } from "react";
-import { Button, Card, Col, Row, Stack, Form } from "react-bootstrap";
+import { Button, Card, Col, Row, Stack } from "react-bootstrap";
 import { RequiredComponent, WarningComponent, SuccessComponent } from "./utlilityComponents";
-import { useNavigate } from "react-router-dom";
-import theme from './theme.json'
+import { BrowserRouter as Router, Route, Routes, Link, useParams, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { KirjaViewerComponent, KuvaViewerComponent } from "./kuvaComponents";
 
+import theme from './theme.json'
 
 
 const EditSeries = (props) => {
   const { sarja_id, onUpdate } = props;
+  const [sarjat, setSarjat] = useState([]);
+  const [isDone, setIsDone] = useState(false);
+
+
+  const { id } = useParams();
   const navigate = useNavigate();
- 
-  const [selectedSeries, setSelectedSeries] = useState({});
+  const location = useLocation();
+  const [selectedSeries, setSelectedSeries] = useState(null);
+  const [allBooksData, setAllBooksData] = useState([]);
+
+
   const [nimi, setNimi] = useState("");
   const [kuvaus, setKuvaus] = useState("");
+  const [relatedKirja, setRelatedKirja] = useState([]);
+  const [relatedKirjaID, setRelatedKirjaID] = useState([]);
 
-  const [allSeries, setAllSeries] = useState([]);
-  
+
+  const [allBooks, setAllBooks] = useState([]);
+  const [selectedBook, setSelectedBook] = useState("");
+
 
   useEffect(() => {
     const fetchSeries = async () => {
-      const f = await fetch("http://localhost:5000/oma_sarja", {
-        credentials: "include",
-      });
+      const f = await fetch(`http://localhost:5000/sarja?sarja_id=${id}`);
       const data = await f.json();
-      const selectedSeries = data.data[0];
+      const selectedSeries = data.data[0]; 
+      const k = await fetch(`http://localhost:5000/sarjan_kirjat?sarja_id=${id}`);
+      const kirjaData = await k.json();
+
       setSelectedSeries(selectedSeries);
       setNimi(selectedSeries.nimi);
       setKuvaus(selectedSeries.kuvaus);
+      setRelatedKirja(kirjaData.data);
+      setRelatedKirjaID(kirjaData.data.map(kirja => kirja.kirja_id));
     };
     fetchSeries();
-  }, [sarja_id]);
+  }, [id]);
   
-  useEffect(() => {
-    const fetchSeries = async () => {
-      const b = await fetch("http://localhost:5000/oma_sarja", {
-        credentials: "include",
-      });
-      const allSeriesData = await b.json();
-      setAllSeries(allSeriesData.data);
-    };
-    fetchSeries();
-  }, []);
-  
+
 
 
   const handleCancelClicked = () => {
-    navigate('/omasarjasivu'); 
+    navigate('/omasarjasivu', { replace: true });
+
+
   };
+  
 
 
 
@@ -54,31 +64,32 @@ const EditSeries = (props) => {
     const response = await fetch("http://localhost:5000/oma_sarja", {
       credentials: "include",
       method: 'DELETE',
-      mode: "cors",
     });
-  
+
     if (response.ok) {
-      navigate('/omasarjasivu');
+      setIsDone(true);
     } else {
       console.error('Failed to delete the series');
     }
   };
-  
 
   const inputStyle = { width: "60%", paddingLeft: "1em" }
 
 
   const [sarjaFilled, setSarjaFilled] = useState(true)
+  const [filesFilled, setFilesFilled] = useState(true)
   const [saveSuccessful, setSaveSuccessful] = useState(false)
 
-  async function updateOwnSeries(updatedOwnSeries) {
+
+
+  async function updateSeries(updatedSeries) {
     const response = await fetch(`http://localhost:5000/oma_sarja`, {
       credentials: "include",
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(updatedOwnSeries),
+      body: JSON.stringify(updatedSeries),
     });
 
     if (response.ok) {
@@ -87,105 +98,258 @@ const EditSeries = (props) => {
       console.error('Failed to update the series');
     }
   }
-
-
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await fetch("http://localhost:5000/kirja");
+      const data = await response.json();
+      setAllBooks(data.data);
+    };
+    fetchBooks();
+  }, []);
   
-  const handleSaveClicked =  async () => {
-    console.log("Save button clicked");
+
+
+  const handleAddBookToSeries = async () => {
+    if (selectedBook) {
+      const response = await fetch("http://localhost:5000/sarjan_kirjat", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          sarja_id: id,
+          kirja_id: selectedBook,
+        }),
+      });
+
+      if (response.ok) {
+     
+        const k = await fetch(`http://localhost:5000/sarjan_kirjat?sarja_id=${id}`);
+        const kirjaData = await k.json();
+        setRelatedKirja(kirjaData.data);
+
+
+        setSelectedBook("");
+      } else {
+        console.error("Failed to add the book to the series");
+      }
+    }
+  };
+
+  const handleSaveClicked = () => {
     if (checkInputs()) {
-      console.log("Inputs are valid");
-      const updatedOwnSeries = {
+      const updatedSeries = {
         set: {
           nimi: nimi,
           kuvaus: kuvaus,
         },
         where: {
-          oma_sarja_id: selectedSeries.oma_sarja_id,
+          sarja_id: selectedSeries.sarja_id,
         },
       };
-      await updateOwnSeries(updatedOwnSeries);
-      onUpdate(); // Lisää tämä rivi ilmoittamaan vanhemmalle komponentille, että sarja on päivitetty
-    } else {
-      console.log("Inputs are invalid");
+      updateSeries(updatedSeries);
     }
   };
 
 
-
   const checkInputs = () => {
     const omaSarjaOK = nimi.length > 0 && kuvaus.length > 0;
+    const fileInputsOK = Array.from(
+      document.getElementsByName("files")
+    ).every((fi) => fi.files.length > 0);
 
     setSarjaFilled(omaSarjaOK);
+    setFilesFilled(fileInputsOK);
 
-    return omaSarjaOK;
+    return omaSarjaOK && fileInputsOK;
   };
 
+  const [editClicked, setEditClicked] = useState(false);
+  
 
   return (
-    <Card border="secondary" className="mb-1" style={{ backgroundColor: theme.input, color: "white" }}>
-      <Card.Body>
-        {!saveSuccessful ? (
-          <>
-            <Card.Title>Muokkaa omaa sarjaa</Card.Title>
-            <Row className="mb-2">
-              <Col>
-                <Stack direction="vertical" gap={3} style={{ textAlign: "center" }}>
+    <>
+      {!editClicked ? (
 
+        <Col>
+          <Row className="mt-5">
+            <Col sm={10} lg={3}>
+              <Card className="mb-4">
+                <div style={{ color: "white", background: "#131415", borderRadius: "inherit" }}>
+                  <Card.Title className="text-center mt-3">Sarjan kirjat</Card.Title>
+                  <Card.Body >
                   
-
-                  <div>
-                    <input
-                      value={nimi}
-                      onChange={(e) => setNimi(e.target.value)}
-                      placeholder="nimi"
-                      style={inputStyle}
-                    />
-                    <RequiredComponent yes />
-                  </div>
-                  <div>
-                    <input
-                      value={kuvaus}
-                      onChange={(e) => setKuvaus(e.target.value)}
-                      placeholder="kuvaus"
-                      style={inputStyle}
-                    />
-                    <RequiredComponent yes />
-                  </div>
-                </Stack>
-              </Col>
-            </Row>
-            <Row className="mb-2">
-            </Row>
-            <Row>
-              <Col>
-                <div className="my-5">
-                  {!sarjaFilled ? (
-                    <WarningComponent text="Vaadittuja tietoja puuttuu" />
-                  ) : (
-                    <></>
-                  )}
-
-                  <Button onClick={(e) => handleSaveClicked()}>Tallenna</Button>{" "}
-                  <Button onClick={(e) => handleCancelClicked()}>
-                    Peruuta
-                  </Button>{" "}
-                  <Button onClick={(e) => handleDeleteClicked()}>Poista</Button>{" "}
+                  <KirjaViewerComponent kirjaId={relatedKirja.map(kirja => kirja.kirja_id).join(',')} />
 
 
 
+
+                  </Card.Body>
                 </div>
-              </Col>
-            </Row>
-          </>
-        ) : (
-          <>
-            <SuccessComponent text="Tallennus onnistui" />
-            <Button onClick={(e) => props.handleLisaaClicked({ selectedSeries })}>Sulje</Button>
-          </>
-        )}
-      </Card.Body>
-    </Card>
+              </Card>
+            </Col>
+            <Col sm={12} lg={8}>
+              <Card
+                border="secondary"
+                style={{ backgroundColor: theme.accent, color: "white" }}
+              >
+                <Card.Body>
+                  <h1 className="text-center mt-3">{selectedSeries?.nimi}</h1>
+                  <hr />
+                  <Card.Text className="text-center mt-3">
+                    <strong>Kuvaus:</strong> {selectedSeries?.kuvaus}
+                  </Card.Text>
+                  <h3>Sarjan kirjat:</h3>
+                  {relatedKirja.length > 0 ? (
+                    <ul>
+                      {relatedKirja.map((kirja) => (
+                        <li key={kirja.kirja_id}>{kirja.nimi}</li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p></p>
+                  )}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+
+
+          <Card
+            className="my-4"
+            border="secondary"
+            style={{ backgroundColor: theme.accent, color: "white" }}
+          >
+            <Card.Title className="text-center mt-3">Toiminnot</Card.Title>
+            <Card.Body className="text-center mt-3">
+              <Button
+                variant="dark"
+                style={{ backgroundColor: theme.button }}
+                onClick={() => setEditClicked(true)}
+              >
+                ✏ Muokkaa
+              </Button>{" "}
+              <span className="mx-3" />
+              {isDone ? (
+                <>
+                  <Card bg="dark" className="px-2 py-5" style={{ color: "white", height: "auto", width: "auto", margin: "20%" }}>
+                    <SuccessComponent text="Poisto onnistui"></SuccessComponent>
+                    <Link to="/omasarjasivu"><Button variant="success">Jatka</Button></Link>
+                  </Card>
+                </>
+              ) : (
+                <>
+                  <Button
+                    variant="danger"
+                    style={{ backgroundColor: theme.accent, color: "red" }}
+                    onClick={() => handleDeleteClicked()}
+                  >
+                    🗑 Poista
+                  </Button>
+                </>
+              )}
+            </Card.Body>
+          </Card>
+          
+        </Col>
+      ) : (
+
+        <Card border="secondary" className="mb-1" style={{ backgroundColor: theme.input, color: "white" }}>
+          <Card.Body>
+            {!saveSuccessful ? (
+              <>
+                <Card.Title>Muokkaa sarjaa</Card.Title>
+                <Row className="mb-2">
+                  <Col>
+                    <Stack direction="vertical" gap={3} style={{ textAlign: "center" }}>
+
+                      <div>
+                        <input
+                          value={nimi}
+                          onChange={(e) => setNimi(e.target.value)}
+                          placeholder="nimi"
+                          style={inputStyle}
+                        />
+                        <RequiredComponent yes />
+                      </div>
+                      <div>
+                        <input
+                          value={kuvaus}
+                          onChange={(e) => setKuvaus(e.target.value)}
+                          placeholder="kuvaus"
+                          style={inputStyle}
+                        />
+                        <RequiredComponent yes />
+                      </div>
+                    </Stack>
+                  </Col>
+                </Row>
+                <Row className="mb-2">
+                  <Col>
+                    <h3>Sarjan kirjat:</h3>
+                    {relatedKirja.length > 0 ? (
+                      <ul>
+                        {relatedKirja.map((kirja) => (
+                          <li key={kirja.kirja_id}>{kirja.nimi}</li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <p></p>
+                    )}
+
+                  </Col>
+                  <Col>
+                    <h3>Lisää kirja sarjaan:</h3>
+                    <select
+                      value={selectedBook}
+                      onChange={(e) => setSelectedBook(e.target.value)}
+                    >
+                      <option value="">Valitse kirja</option>
+                      {allBooks.map((book) => (
+                        <option key={book.kirja_id} value={book.kirja_id}>
+                          {book.nimi}
+                        </option>
+                      ))}
+                    </select>
+                    <Button onClick={handleAddBookToSeries}>Lisää kirja</Button>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <div className="my-5">
+                      {!sarjaFilled ? (
+                        <WarningComponent text="Vaadittuja tietoja puuttuu" />
+                      ) : (
+                        <></>
+                      )}
+
+                      <Button onClick={(e) => handleSaveClicked()}>Tallenna</Button>{" "}
+                      <Button onClick={(e) => handleCancelClicked()}>
+                        Peruuta
+                      </Button>{" "}
+
+
+
+
+                    </div>
+                  </Col>
+                </Row>
+              </>
+            ) : (
+              <>
+                <SuccessComponent text="Tallennus onnistui" />
+                <Button onClick={(e) => handleCancelClicked()}>
+                  Sulje
+                </Button>
+
+              </>
+            )}
+          </Card.Body>
+        </Card>
+      )}
+    </>
   );
 };
 
-export { EditSeries }
+export { EditSeries };
--- frontend/kirja-arkisto/src/omasarja/kuvaComponents.js
+++ frontend/kirja-arkisto/src/omasarja/kuvaComponents.js
@@ -1,20 +1,135 @@
-import { useState } from "react"
-import { Button, Image,  Stack } from "react-bootstrap"
-import theme from './theme.json'
+import { useState, useEffect } from "react";
+import { Button, Image, Stack } from "react-bootstrap";
+import theme from "./theme.json";
+
+const KirjaViewerComponent = ({ kirjaId }) => {
+  // Set the initial state for the currentImage
+  const [currentImage, setCurrentImage] = useState(null);
+
+  
+  const [rollIndex, setRollIndex] = useState(0);
+  const width = 5;
+  const BtnStyle = { backgroundColor: theme.button };
+  const [bookData, setBookData] = useState(null);
+
+  useEffect(() => {
+    fetchBookData();
+  }, []);
+
+  const fetchBookData = async () => {
+    try {
+      // Use the first related book ID from the props
+     
+  
+      const response = await fetch(`http://localhost:5000/kirja_kaikella`);
+      const data = await response.json();
+      if (data.status === "OK") {
+        setBookData(data.data);
+        setCurrentImage(data.data[0].kuvat[0]);
+      }
+    } catch (error) {
+      console.error("Error fetching book data:", error);
+    }
+  };
+  const handleImageClick = (image) => {
+    setCurrentImage(image);
+  };
+
+  const handleIncrease = () => {
+    if (rollIndex < bookData[0].kuvat.length - width) setRollIndex(rollIndex + 1);
+  };
+
+  const handleDecrease = () => {
+    if (rollIndex > 0) setRollIndex(rollIndex - 1);
+  };
+
+  if (!bookData) {
+    return <div>Loading...</div>;
+  }
+
+  // Sort the kirjat array using the sortByJärjestysnumero function
+  const sortedKirjat = sortByJärjestysnumero(bookData[0].kuvat);
+
+  // Generate the previewList using the mapKirjaToPreviews function
+  const previewList = mapKirjaToPreviews(sortedKirjat, "http://localhost:5000/kuvatiedosto?kuva=", currentImage, setCurrentImage);
+
+  const remainingImages = bookData[0].kuvat.length - (rollIndex + width);
+
+  return (
+    <div>
+      <div className="mx-auto" style={{ width: "100%", height: "auto", marginBottom: "1em" }}>
+        <a onClick={(e) => window.open(`http://localhost:5000/kuvatiedosto?kuva=${currentImage.kuva}`, "_blank").focus()} style={{ cursor: "pointer" }}>
+          <Image src={`http://localhost:5000/kuvatiedosto?kuva=${currentImage.kuva}`} fluid style={{ width: "100%" }} />
+        </a>
+      </div>
+      <Stack direction="horizontal" gap={1}>
+        <Button onClick={(e) => handleDecrease()} className="btn btn-dark" style={BtnStyle}>
+          {"< " + rollIndex}
+        </Button>
+        {previewList}
+        <Button onClick={(e) => handleIncrease()} className="btn btn-dark" style={BtnStyle}>
+          {"> " + remainingImages}
+        </Button>
+      </Stack>
+      <div style={{ textAlign: "left" }}>
+        <p>Nimi: {bookData[0].nimi}</p>
+        <p>Järjestysnumero: {bookData[0].jarjestysnumero}</p>
+        <p>Kuvaus: {bookData[0].kuvaus}</p>
+        <p>Kirjailijat: {bookData[0].kirjailijat}</p>
+        <p>Ensipainosvuosi: {bookData[0].ensipainosvuosi}</p>
+
+      </div>
+    </div>
+  );
+};
+const sortByJärjestysnumero = (kirjat) => {
+    return kirjat.sort((a, b) => {
+      if (a.jarjestysnumero < b.jarjestysnumero) return -1;
+      if (a.jarjestysnumero > b.jarjestysnumero) return 1;
+      return 0;
+    });
+  };
+  
+  const mapKirjaToPreviews = (list, kirjaSrc, currentImage, setCurrentImage) => {
+    let previewList = list.map((n, index) => {
+      let style = { width: "20%" };
+      let clickedStyle = { width: "20%", border: "2px solid black", borderRadius: "5px" };
+  
+      if (currentImage.kuva_id === n.kuva_id) {
+        return (
+          <div key={index} onClick={(e) => setCurrentImage(n)} style={clickedStyle}>
+            <Image src={kirjaSrc + n.kuva} thumbnail fluid />
+          </div>
+        );
+      }
+      return (
+        <div key={index} onClick={(e) => setCurrentImage(n)} style={style}>
+          <Image src={kirjaSrc + n.kuva} thumbnail fluid />
+        </div>
+      );
+    });
+    return previewList;
+  };
+
+
+  
+
 
 const KuvaViewerComponent = (props) => {
 
+    // Tietoa esikatselulle ja pikkukuvien kelaukselle
     const [clickedPic, setClickedPic] = useState(props.kuvat[0]);
     const [rollIndex, setRollIndex] = useState(0);
 
     const width = 5
 
-
+    // Jos kirjalla ei ole kuvia, palautetaan tyhjä komponentti
     if (!props.kuvat || props.kuvat.length == 0) return (<></>)
 
     let kuvaSrc = "http://localhost:5000/kuvatiedosto?kuva=";
     let kuvat = props.kuvat
 
+    // kelaavat pikkukuvia nappeja painettaessa
     const handleIncrease = () => {
         if (rollIndex < kuvat.length - width) setRollIndex(rollIndex + 1);
     }
@@ -22,9 +137,14 @@ const KuvaViewerComponent = (props) => {
         if (rollIndex > 0) setRollIndex(rollIndex - 1);
     }
 
+    // Järjestetään kuvat tyypin mukaan (etukansi, takakansi, muut)
     let kuvatSorted = sortByTyyppi(kuvat)
 
-    let previewList = mapKuvaToPreviews(kuvaSrc, clickedPic, setClickedPic)
+    // Rajataan näkyviin tulevat pikkukuvat indeksin ja leveyden perusteella
+    let croppedlist = kuvatSorted.slice(rollIndex, rollIndex + width)
+
+    // Mapataan rajattu lista pikkukuviksi
+    let previewList = mapKuvaToPreviews(croppedlist, kuvaSrc, clickedPic, setClickedPic)
 
     let remainingImages = kuvat.length - rollIndex - width
     if (remainingImages < 0) remainingImages = 0
@@ -84,19 +204,19 @@ const mapKuvaToPreviews = (list, kuvaSrc, clickedPic, setClickedPic) => {
 
 const ValokuvaViewerComponent = (props) => {
 
-  
+    // Tietoa esikatselulle ja pikkukuvien kelaukselle
     const [clickedPic, setClickedPic] = useState(props.valokuvat[0]);
     const [rollIndex, setRollIndex] = useState(0);
 
     const width = 5
 
-
+    // Jos oamlla kirjalla ei ole valokuvia, palautetaan tyhjä komponentti
     if (!props.valokuvat || props.valokuvat.length == 0) return (<></>)
 
     let kuvaSrc = "http://localhost:5000/valokuvatiedosto?valokuva=";
     let valokuvat = props.valokuvat
 
-  
+    // kelaavat pikkukuvia nappeja painettaessa
     const handleIncrease = () => {
         if (rollIndex < valokuvat.length - width) setRollIndex(rollIndex + 1);
     }
@@ -104,7 +224,14 @@ const ValokuvaViewerComponent = (props) => {
         if (rollIndex > 0) setRollIndex(rollIndex - 1);
     }
 
-    let previewList = mapValokuvaToPreviews(kuvaSrc, clickedPic, setClickedPic)
+    // Järjestetään valokuvat sivunumeron mukaan (kasvava)
+    let valokuvatSorted = sortBySivunumero(valokuvat)
+
+    // Rajataan näkyviin tulevat pikkukuvat indeksin ja leveyden perusteella
+    let croppedlist = valokuvatSorted.slice(rollIndex, rollIndex + width)
+
+    // Mapataan rajattu lista pikkukuviksi
+    let previewList = mapValokuvaToPreviews(croppedlist, kuvaSrc, clickedPic, setClickedPic)
 
     let remainingImages = valokuvat.length - rollIndex - width
     if (remainingImages < 0) remainingImages = 0
@@ -135,6 +262,13 @@ const ValokuvaViewerComponent = (props) => {
     )
 }
 
+const sortBySivunumero = (valokuvat) => {
+    return valokuvat.sort((a, b) => {
+        if (a.sivunumero < b.sivunumero) return -1
+        if (a.sivunumero > b.sivunumero) return 1
+        return 0
+    });
+}
 
 const mapValokuvaToPreviews = (list, kuvaSrc, clickedPic, setClickedPic) => {
     let previewList = list.map((n, index) => {
@@ -157,4 +291,4 @@ const mapValokuvaToPreviews = (list, kuvaSrc, clickedPic, setClickedPic) => {
     return previewList
 }
 
-export {KuvaViewerComponent, ValokuvaViewerComponent}
+export {KuvaViewerComponent, ValokuvaViewerComponent, KirjaViewerComponent}
